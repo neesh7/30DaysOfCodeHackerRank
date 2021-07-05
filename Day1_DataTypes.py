0e7fa6daf96fd3ea45ba1628ae09c00035c76d7d
neesh7 <76041032+neesh7@users.noreply.github.com>
2021-07-05 22:35:59 +0530

    Add files via upload

---
+++ Day1_DataTypes.py
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x = int(input())
+y = float(input())
+z =input()
+# x = 12
+# y = 4.0
+# z = "is the best place to learn and practice coding!"
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+x=x+i
+y=y+d
+# z=s+z
+# Print the sum of the double variables on a new line.
+print(x)
+print(y)
+print(s+z)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
